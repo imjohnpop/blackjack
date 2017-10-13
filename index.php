22936ea62e959211a3444bf9022bb20bc70f1341
LuisVuitton <holbikl@gmail.com>
2017-10-13 12:49:03 +0200

    ...

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="table.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <title>Black Jack</title>
 </head>
 <body>
@@ -32,6 +33,8 @@
         <div id="deck"></div>
 
         <div class="half-circle"></div>
+
+        <button class="buttonStart btn btn-succes" id="buttonStart">Start</button>
         
         <?php foreach ($main_deck as $value): ?>
             <?= $value; ?>
@@ -42,42 +45,66 @@
 
     
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <script>
     $(".card").click(function() {
-        $(this).toggleClass("flipper")
-    })
+            $(this).toggleClass("flipper")
+        })
 
-    var deck = $('#deck').offset();
+        var deck = $('#deck').offset();
     $('.card').css({
         'top': deck.top - 51,
         'left': deck.left + 50,
     });
 
+        for ($i=1; $i<5; $i++) {
+            if (($i%2)==0) {
+            //dealer      
+            var card = $(".deck .card").last();
 
-    for ($i=1; $i<5; $i++) {
-        if (($i%2)==0) {
-        //dealer      
-        var card = $(".deck .card").last();
 
-        
+/* ---------------------------------------------------------------------------*/
+            var dealerOffset = $('#dealer').offset()
+            
+            $('.buttonStart').click(function() {                                               
 
-        card.detach();
-        $('#dealer').append(card);
-        //-------
-        } else {
-        //hrac
-        var card = $(".deck .card").last();
+                $('#dealer').append(card);
+                
+                var btnOffset = $(this).offset(); 
 
-        
+                $('#card');
+            
+            $('#card').animate({
+                'top': dealerOffset.top,
+                'left':dealerOffset.left
+            }, 1000, function () {
+                $('#card').remove();
 
-        card.detach();
-        $('#player').append(card);
+
+/* ---------------------------------------------------------------------------*/
+            
+
+            card.detach();
+            $('#dealer').append(card);
+            //-------
+            } else {
+            //hrac
+            var card = $(".deck .card").last();
+
+            
+
+            card.detach();
+            $('#player').append(card);
+            }
+            //-------
         }
-        //-------
-    }
 
 </script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
 </body>
 </html>
